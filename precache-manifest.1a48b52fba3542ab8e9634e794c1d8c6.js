b464f2457db85dce81bbed9b5c9ce0ef217c699e
DiKeySnakes <dikeysnakes@gmail.com>
2023-02-12 17:56:11 +0200

    Updates

---
+++ precache-manifest.1a48b52fba3542ab8e9634e794c1d8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1907f83574ce1e8abe23",
+    "url": "/random-quote-machine-project/static/css/main.18bca82f.chunk.css"
+  },
+  {
+    "revision": "1907f83574ce1e8abe23",
+    "url": "/random-quote-machine-project/static/js/main.1907f835.chunk.js"
+  },
+  {
+    "revision": "d25c88274188aadfa714",
+    "url": "/random-quote-machine-project/static/js/1.d25c8827.chunk.js"
+  },
+  {
+    "revision": "f498fb625d540359a0bf",
+    "url": "/random-quote-machine-project/static/js/runtime~main.f498fb62.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/random-quote-machine-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "36c50a233b1a9fcad76d382613a66b11",
+    "url": "/random-quote-machine-project/index.html"
+  }
+];
