0966f0a40efaf28b9803b341911c0949a1321c3e
DiKeySnakes <dikeysnakes@gmail.com>
2023-02-12 17:54:47 +0200

    Clean app, add npm axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios
+      .get('https://dummyjson.com/quotes/random')
+      .then((response) => {
+        const quote = response.data;
+
+        setQuote(quote.quote);
+        setAuthor(quote.author);
+        console.log(quote.quote);
+        console.log(quote.author);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const text = quote;
+  const person = author;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='app'>
+      <div id='quote-box' className='card'>
+        <p id='text' className='heading'>
+          {text}
+        </p>
+        <h2 id='author'>{person}</h2>
+        <button id='new-quote' className='button' onClick={fetchAdvice}>
+          <span>New Quote</span>
+        </button>
+        <a id='tweet-quote' href='twitter.com/intent/tweet' target='_blank'>
+          Tweet Quote
+        </a>
+      </div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
